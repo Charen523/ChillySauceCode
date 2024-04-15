5dba754d3703a5608559640c8e5e1be7de618ce2
Chloe <144107013+Charen523@users.noreply.github.com>
2024-04-15 16:41:14 +0900

    BGM 마무리
    AudioManager 코드에 한번 재생된 배경음악이 끊김없이 Scene 변경 시까지 따라올 수 있도록 해줌.

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance;
+
     public AudioClip clip;
 
     AudioSource audioSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);  
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
