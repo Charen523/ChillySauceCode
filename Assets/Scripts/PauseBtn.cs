15f49c8855b82abd077a46c919af37bbef50bbba
Mrdosim <127918879+Mrdosim@users.noreply.github.com>
2024-04-15 15:53:09 +0900

    일시정지 메뉴 생성
    나중에 음량 조절 바 / 게임 종료 버튼을 위해 일시 정지 메뉴 생성

---
+++ Assets/Scripts/PauseBtn.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseBtn : MonoBehaviour
+{
+    public GameObject PauseMenu;
+
+    public bool isPaused = false;
+    public void PushPauseMenu()
+    {
+        if (!isPaused)
+        {
+            // 일시 정지 버튼을 눌러 isPaused = false면 게임을 멈춤
+            Time.timeScale = 0.0f;
+            isPaused = true;
+            // 비 활성화 시켜두었던 PauseMenu가 등장
+            PauseMenu.SetActive(true);
+
+        }
+        else
+        {
+            // isPaused = true 면 다시 게임을 재개
+            Time.timeScale = 1.0f;
+            isPaused = false;
+            // 활성화 되었던 PasueMenu가 다시 비활성화
+            PauseMenu.SetActive(false);
+
+        }
+    }
+}
