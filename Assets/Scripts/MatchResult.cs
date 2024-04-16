d44067eb1a6155d48dd69cfc1cd2551d98d1c6f6
Chloe <144107013+Charen523@users.noreply.github.com>
2024-04-16 11:08:11 +0900

    카드 선택 시 성공, 실패 사인 보여주기
    Enable 작업에 약간 문제가 있는것 같아 대기 상태(게임 실행 전)을 visible하게 두고 Start 함수 작동 시 숨기도록 함.

--- Assets/Scripts/MatchResult.cs
+++ Assets/Scripts/MatchResult.cs
@@ -6,8 +6,8 @@ using UnityEngine.UI;
 public class MatchResult : MonoBehaviour
 {
     public GameManager manager; //삭제 예정.
-    public Image matchBackground; //GM으로 이동해서 재선언.
-    public Text ResultTxt; //GM으로 이동해서 재선언.
+    public Image matchPanel; //GM으로 이동해서 재선언.
+    public Text matchTxt; //GM으로 이동해서 재선언.
     
 
     Color FailColor = new Color(255 / 255f, 119 / 255f, 119 / 255f); //실패시 이미지 배경색.
@@ -25,34 +25,15 @@ public class MatchResult : MonoBehaviour
     {
         if (manager.firstCard.idx == manager.secondCard.idx)
         {
-            matchBackground.enabled = true;
-
-            switch (manager.firstCard.idx % 4)
-            {
-                case 0:
-                    ResultTxt.text = "김영선!";
-                    break;
-                case 1:
-                    ResultTxt.text = "박재균!";
-                    break;
-                case 2:
-                    ResultTxt.text = "이승영!";
-                    break;
-                case 3:
-                    ResultTxt.text = "정승연!";
-                    break;
-                default:
-                    Debug.Log("인덱스 오류.");
-                    break;
-            }
+            
         }
         else
         {
-            ResultTxt.text = "실패...";
+            
         }
 
         yield return new WaitForSeconds(1f);
 
-        matchBackground.enabled = true;
+        matchPanel.enabled = true;
     }
 }
