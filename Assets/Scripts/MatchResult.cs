628116d4ffe586d746afe3c6260f04da8d20c7ff
Chloe <144107013+Charen523@users.noreply.github.com>
2024-04-16 10:37:20 +0900

    MatchResult.cs
    1. 필요에 따라 text 바꾸기
    2. 실패는 붉은 배경으로 바꾸기
    3. 성공은 녹색 배경으로 바꾸기
    4. 1초 후 판넬 없애기

--- Assets/Scripts/MatchResult.cs
+++ Assets/Scripts/MatchResult.cs
@@ -5,23 +5,54 @@ using UnityEngine.UI;
 
 public class MatchResult : MonoBehaviour
 {
-    public GameManager manager;
-    public Image image;
+    public GameManager manager; //삭제 예정.
+    public Image matchBackground; //GM으로 이동해서 재선언.
+    public Text ResultTxt; //GM으로 이동해서 재선언.
+    
 
-    Color FailColor = new Color(255 / 255f, 119 / 255f, 119 / 255f);
-    Color SuccessColor = new Color(154 / 255f, 255 / 255f, 154 / 255f);
+    Color FailColor = new Color(255 / 255f, 119 / 255f, 119 / 255f); //실패시 이미지 배경색.
+    Color SuccessColor = new Color(154 / 255f, 255 / 255f, 154 / 255f);//성공시 이미지 배경색.
 
     // Start is called before the first frame update
     void Start()
     {
-        Image image = GetComponent<Image>();
+        Image image = GetComponent<Image>(); //GameManager의 Start함수에 필요.
+        StartCoroutine(MatchPanelDelay());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (true)
 
-        image.color = FailColor;
+    IEnumerator MatchPanelDelay()
+    {
+        if (manager.firstCard.idx == manager.secondCard.idx)
+        {
+            matchBackground.enabled = true;
+
+            switch (manager.firstCard.idx % 4)
+            {
+                case 0:
+                    ResultTxt.text = "김영선!";
+                    break;
+                case 1:
+                    ResultTxt.text = "박재균!";
+                    break;
+                case 2:
+                    ResultTxt.text = "이승영!";
+                    break;
+                case 3:
+                    ResultTxt.text = "정승연!";
+                    break;
+                default:
+                    Debug.Log("인덱스 오류.");
+                    break;
+            }
+        }
+        else
+        {
+            ResultTxt.text = "실패...";
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        matchBackground.enabled = true;
     }
 }
