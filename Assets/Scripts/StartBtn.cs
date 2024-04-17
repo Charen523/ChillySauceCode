ecac97c25e8548996215e1baeec306e394aca501
Mrdosim <127918879+Mrdosim@users.noreply.github.com>
2024-04-17 20:10:52 +0900

    endTxt뒤에 판 완성
    background  크기 수정

--- Assets/Scripts/StartBtn.cs
+++ Assets/Scripts/StartBtn.cs
@@ -2,14 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartBtn : MonoBehaviour
 {
-    
+    private float time;
+
     void Start()
     {
         LevelManager.Instance.LevelCheck();
         
+    }
+
+    private void Update()
+    {
+        if(time < 1.0f)
+        {
+            GetComponentInChildren<Text>().color = new Color(1, 1, 1, 1 - time);
+        }
+        else
+        {
+            GetComponentInChildren<Text>().color = new Color(1, 1, 1, time);
+            if(time > 1.0f)
+            {
+                time = 0f;
+            }
+        }
     }
 
     public void StartGame()
@@ -25,4 +43,6 @@ public class StartBtn : MonoBehaviour
             LevelManager.Instance.OpenCanvas();
         }
     }
+
+
 }
