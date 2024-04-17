b5cb1f5baf339a277f6eb427112500b070e1c287
Mrdosim <127918879+Mrdosim@users.noreply.github.com>
2024-04-17 17:56:42 +0900

    오디오 볼륨 조절바 구현
    1. 스타트씬에서 조절한 볼륨 조절바가 메인씬으로 넘어가면 조절바의 상태만 초기화 되는 오류 발생

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance;
+
+    public AudioMixer AudioMixer;
+    public Slider BGMSilder;
+    public Slider SFXSilder;
+
 
     public AudioClip[] bgmClips;
     public AudioClip[] sfxClips;
 
     public AudioSource[] audioSource;
 
+    float bgmVolume;
+    float sfxVolume;
+
+    float bgmSound;
+    float sfxSound;
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);  
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
@@ -27,7 +40,18 @@ public class AudioManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      audioSource = GetComponentsInChildren<AudioSource>();
+        audioSource = GetComponentsInChildren<AudioSource>();
+
+        // 볼륨 값 불러오기
+        bgmVolume = PlayerPrefs.GetFloat("BGMVolume");
+        sfxVolume = PlayerPrefs.GetFloat("SFXVolume");
+
+        BGMSilder.value = bgmSound;
+        SFXSilder.value = sfxSound;
+
+        // 오디오 믹서에 볼륨 설정
+        SetBgmVolum(bgmVolume);
+        SetSfxVolum(sfxVolume);
 
         audioSource[0].clip = bgmClips[0];
         audioSource[0].Play();
@@ -36,18 +60,28 @@ public class AudioManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PauseBGM();
     }
-    public void PauseBGM()
+    //Bgm 볼륨 조절
+    public void SetBgmVolum(float volume)
     {
-        if (PauseBtn.isPaused == true)
-        {
-            audioSource[0].Pause();
-        }
-        else if (PauseBtn.isPaused == false)
-        {
-            audioSource[0].UnPause();
-        }
+        float bgSound = BGMSilder.value;
+
+        if (bgSound == -40f) AudioMixer.SetFloat("BGM", -80);
+        else AudioMixer.SetFloat("BGM", bgSound);
+
+
+
     }
+    //효과음 볼륨 조절
+    public void SetSfxVolum(float volume)
+    {
+        float sfxSound = SFXSilder.value;
+
+        if (sfxSound == -40f) AudioMixer.SetFloat("SFX", -80);
+        else AudioMixer.SetFloat("SFX", sfxSound);
+
+    }
+
+
 
 }
